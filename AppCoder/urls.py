3d305e898ded8ddff703be2dd78054a5cd1373fe
Eduardo Valles <valles.eduardo@gmail.com>
2025-01-12 12:26:47 -0300

    edu1

--- AppCoder/urls.py
+++ AppCoder/urls.py
@@ -9,8 +9,8 @@ urlpatterns = [
     path('estudiantes/', lista_estudiantes, name='lista_estudiantes'),
     path("cargar_profesor/", cargar_profesor, name = "cargar_profesor"),
     path("profesores/", lista_profesores, name= "lista_profesores"),
+     path("cargar_curso/", cargar_curso, name= "cargar_curso"),
     path("cursos/", lista_cursos, name= "lista_cursos"),
-    path("cursos/", cargar_curso, name= "cargar_curso"),
     path('buscar/', buscar, name='buscar'),
 ]
 
